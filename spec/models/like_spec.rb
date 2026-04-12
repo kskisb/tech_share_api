44f112e4a3d24b5db06dbfef6701f729c45e15ce
kskisb <isbkosuke@gmail.com>
2026-04-12 22:46:28 +0900

    feat: モデルとテスト作成

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: "Alice", email: "alice_like_spec@example.com", password_digest: "digest") }
+  let(:post_record) { Post.create!(user: user, title: "Post title", body: "Post body") }
+
+  describe "associations" do
+    it "ユーザーに属すること" do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+
+    it "投稿に属すること" do
+      association = described_class.reflect_on_association(:post)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe "validations" do
+    subject(:like) { described_class.new(user: user, post: post_record) }
+
+    it "有効ないいねであること" do
+      expect(like).to be_valid
+    end
+
+    it "ユーザーがなければ無効になること" do
+      like.user = nil
+      expect(like).to be_invalid
+      expect(like.errors[:user]).to be_present
+    end
+
+    it "投稿がなければ無効になること" do
+      like.post = nil
+      expect(like).to be_invalid
+      expect(like.errors[:post]).to be_present
+    end
+
+    it "同一ユーザーの同一投稿への重複いいねは無効になること" do
+      described_class.create!(user: user, post: post_record)
+      duplicated_like = described_class.new(user: user, post: post_record)
+
+      expect(duplicated_like).to be_invalid
+      expect(duplicated_like.errors[:user_id]).to be_present
+    end
+  end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it "post_tagsを複数持つこと" do
+      association = described_class.reflect_on_association(:post_tags)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it "投稿をpost_tags経由で複数持つこと" do
+      association = described_class.reflect_on_association(:posts)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:post_tags)
+    end
+  end
+
+  describe "validations" do
+    subject(:tag) { described_class.new(name: "rails") }
+
+    it "有効なタグであること" do
+      expect(tag).to be_valid
+    end
+
+    it "名前がなければ無効になること" do
+      tag.name = nil
+      expect(tag).to be_invalid
+      expect(tag.errors[:name]).to be_present
+    end
+
+    it "重複した名前は無効になること" do
+      described_class.create!(name: "rails")
+      duplicated_tag = described_class.new(name: "rails")
+
+      expect(duplicated_tag).to be_invalid
+      expect(duplicated_tag.errors[:name]).to be_present
+    end
+  end
 end

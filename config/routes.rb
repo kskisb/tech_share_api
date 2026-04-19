198d164db7ed0a0f02ad913aa314f3d4d4145087
kskisb <isbkosuke@gmail.com>
2026-04-19 17:31:04 +0900

    chore: rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      post '/auth/signup', to: 'users#create'
+      post "/auth/signup", to: "users#create"
     end
   end
 end
